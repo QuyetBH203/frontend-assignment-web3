f936c2ff426e5794c05b9d1ebdc463b52b3ce7d6
QuyetCN05 <buihuuquyet203@gmail.com>
2024-09-04 08:24:07 +0700

    add withdrawn state

--- src/setting/store/withDrawnState.ts
+++ src/setting/store/withDrawnState.ts
@@ -1,12 +1,7 @@
 import { create } from 'zustand'
+import { CounterState } from './counterState'
 
-export interface CounterState {
-  count: number
-  increase: () => void
-  decrease: () => void
-}
-
-export const useCounterStore = create<CounterState>((set) => ({
+export const useWithDrawnState = create<CounterState>((set) => ({
   count: 0,
   increase: () => set((state) => ({ count: state.count + 1 })),
   decrease: () => set((state) => ({ count: state.count - 1 }))
